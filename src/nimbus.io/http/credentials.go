3e1bb48a568d3d11c19e4606371cc81a8c678d74
dougfort <dougfort@fog.(none)>
2012-08-16 17:50:34 -0400

    initial parse of credentials

--- src/nimbus.io/http/credentials.go
+++ src/nimbus.io/http/credentials.go
@@ -1,9 +1,66 @@
 package http
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"strconv"
+	"strings"
+)
+
 type Credentials struct {
 	name      string
 	authKeyId int
 	authKey   []byte
 }
 
+func (credentials *Credentials) Equal(other *Credentials) bool {
+	return credentials.name == other.name &&
+		credentials.authKeyId == other.authKeyId &&
+		bytes.Equal(credentials.authKey, other.authKey)
+}
 
+func LoadCredentials(reader io.Reader) (*Credentials, error) {
+	var line string
+	var err error
+	var fields []string
+
+	credentials := Credentials{}
+	bufferedReader := bufio.NewReader(reader)
+
+	line, err = bufferedReader.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+	fields = strings.Fields(line)
+	if len(fields) != 2 || fields[0] != "Username" {
+		return nil, errors.New("can't parse Username")
+	}
+	credentials.name = fields[1]
+
+	line, err = bufferedReader.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+	fields = strings.Fields(line)
+	if len(fields) != 2 || fields[0] != "AuthKeyId" {
+		return nil, errors.New("can't parse AuthKeyId")
+	}
+	credentials.authKeyId, err = strconv.Atoi(fields[1])
+	if err != nil {
+		return nil, err
+	}
+
+	line, err = bufferedReader.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+	fields = strings.Fields(line)
+	if len(fields) != 2 || fields[0] != "AuthKey" {
+		return nil, errors.New("can't parse AuthKey")
+	}
+	credentials.authKey = []byte(fields[1])
+
+	return &credentials, nil 
+}
---
+++ src/nimbus.io/http/credentials_test.go
@@ -0,0 +1,39 @@
+package http
+
+import (
+	"strings"
+	"testing"
+)
+
+type testCredentialsEntry struct {
+	testString  string
+	credentials Credentials
+}
+
+// test data generated from python library lumberyard
+var testCredentialsData = []testCredentialsEntry{
+	testCredentialsEntry{
+		`Username motoboto-benchmark-000
+AuthKeyId 1
+AuthKey 4TVjaSkh5GNENBqs+GX2OUrDQlofOgzP/0QB+F1+TYY
+`,		
+		Credentials{
+			"motoboto-benchmark-000",
+			1,
+			[]byte("4TVjaSkh5GNENBqs+GX2OUrDQlofOgzP/0QB+F1+TYY"),
+		},
+	},
+}
+
+func TestLoadCredentials(t *testing.T) {
+	for _, entry := range testCredentialsData {
+		reader := strings.NewReader(entry.testString)
+		credentials, err := LoadCredentials(reader)
+		if err != nil {
+			t.Fatalf("error %s, %v", err, entry)
+		}
+		if !credentials.Equal(&entry.credentials) {
+			t.Fatalf("credentials mismatch %v, %v", credentials, entry)
+		}
+	}
+}
