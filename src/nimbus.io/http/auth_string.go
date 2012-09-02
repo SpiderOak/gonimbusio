e1d986aae3dad9d9c5cc331cebf3342614e49a4f
Doug Fort <doug.fort@gmail.com>
2012-09-02 15:15:54 -0400

    unescape path before computing auth string

--- src/nimbus.io/http/auth_string.go
+++ src/nimbus.io/http/auth_string.go
@@ -4,14 +4,22 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"fmt"
+	"net/url"
+
 )
 
 func ComputeAuthString(credentials *Credentials, method string, timestamp int64,
-	path string) string {
+	path string) (string, error) {
+
+	rawPath, err := url.QueryUnescape(path); if err != nil {
+		return "", err
+	}
 
 	message := fmt.Sprintf("%s\n%s\n%d\n%s", credentials.Name, method,
-		timestamp, path)
+		timestamp, rawPath)
 	h := hmac.New(sha256.New, credentials.AuthKey)
 	h.Write([]byte(message))
-	return fmt.Sprintf("NIMBUS.IO %d:%x", credentials.AuthKeyId, h.Sum(nil))
+	authString := fmt.Sprintf("NIMBUS.IO %d:%x", credentials.AuthKeyId, 
+		h.Sum(nil))
+	return authString, nil
 }
