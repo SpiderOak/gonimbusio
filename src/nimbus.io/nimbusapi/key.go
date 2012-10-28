dfcc0ccf8862ec6a491ae810c29b001bb2451d2d
Doug Fort <doug.fort@gmail.com>
2012-10-28 19:23:36 -0400

    added conjoined operations to nimbusapi

--- src/nimbus.io/nimbusapi/key.go
+++ src/nimbus.io/nimbusapi/key.go
@@ -9,6 +9,104 @@ import (
 	"io/ioutil"
 )
 
+func StartConjoined(requester Requester, collectionName string, key string) (
+	string, error) {
+	method := "POST"
+	hostName := requester.CollectionHostName(collectionName)
+	path := fmt.Sprintf("/conjoined/%s?action=start", url.QueryEscape(key))
+
+	response, err := requester.Request(method, hostName, path, nil) 
+	if err != nil {
+		return "", err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		err = fmt.Errorf("POST %s %s failed (%d) %s", hostName, path, 
+			response.StatusCode, response.Body)
+		return "", err
+	}
+
+	defer response.Body.Close()
+	responseBody, err := ioutil.ReadAll(response.Body); if err != nil {
+		return "", err
+	}
+	var conjoinedMap map[string]string
+	err = json.Unmarshal(responseBody, &conjoinedMap); if err != nil {
+        return "", err
+    }
+
+    return conjoinedMap["conjoined_identifier"], nil
+}
+
+func AbortConjoined(requester Requester, collectionName string, key string, 
+	conjoinedIdentifier string) error {
+	method := "POST"
+	hostName := requester.CollectionHostName(collectionName)
+	path := fmt.Sprintf("/conjoined/%s?action=abort&conjoined_identifier=%s", 
+		url.QueryEscape(key), conjoinedIdentifier)
+
+	response, err := requester.Request(method, hostName, path, nil) 
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		err = fmt.Errorf("POST %s %s failed (%d) %s", hostName, path, 
+			response.StatusCode, response.Body)
+		return err
+	}
+
+	defer response.Body.Close()
+	responseBody, err := ioutil.ReadAll(response.Body); if err != nil {
+		return err
+	}
+	var conjoinedMap map[string]string
+	err = json.Unmarshal(responseBody, &conjoinedMap); if err != nil {
+        return err
+    }
+
+    if conjoinedMap["success"] != "true" {
+    	return fmt.Errorf("conjoined action=abort returned %s", 
+    		conjoinedMap["success"])
+    }
+
+    return nil
+}
+func FinishConjoined(requester Requester, collectionName string, key string, 
+	conjoinedIdentifier string) error {
+	method := "POST"
+	hostName := requester.CollectionHostName(collectionName)
+	path := fmt.Sprintf("/conjoined/%s?action=finish&conjoined_identifier=%s", 
+		url.QueryEscape(key), conjoinedIdentifier)
+
+	response, err := requester.Request(method, hostName, path, nil) 
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		err = fmt.Errorf("POST %s %s failed (%d) %s", hostName, path, 
+			response.StatusCode, response.Body)
+		return err
+	}
+
+	defer response.Body.Close()
+	responseBody, err := ioutil.ReadAll(response.Body); if err != nil {
+		return err
+	}
+	var conjoinedMap map[string]string
+	err = json.Unmarshal(responseBody, &conjoinedMap); if err != nil {
+        return err
+    }
+
+    if conjoinedMap["success"] != "true" {
+    	return fmt.Errorf("conjoined action=abort returned %s", 
+    		conjoinedMap["success"])
+    }
+
+    return nil
+}
+
 func Archive(requester Requester, collectionName string, key string, 
 	requestBody io.Reader) (string, error) {
 	method := "POST"
